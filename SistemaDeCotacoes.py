e2fb212aa6343e256e5e671319c07ed65c7e0ae2
henricodias <henrico.diass@gmail.com>
2022-04-26 20:50:20 -0300

    Continuação do layout

--- SistemaDeCotacoes.py
+++ SistemaDeCotacoes.py
@@ -11,6 +11,12 @@ listaMoedas = ['USD', 'BTC']
 def BuscarCotacao():
     pass
 
+def SelecionarArquivo():
+    pass
+
+def AtualizarCotacoes():
+    pass
+
 labelCotacaoMoeda = tk.Label(text='Cotação de uma moeda específica', borderwidth=2, relief='solid')
 labelCotacaoMoeda.grid(row=0, column=0, padx=10, pady=10, sticky='NSEW', columnspan=3)
 
@@ -38,9 +44,31 @@ labelCotacaoVariasMoedas.grid(row=4, column=0, padx=10, pady=10, sticky='NSEW', 
 labelSelecionarArquivo = tk.Label(text='Selecione o arquivo para consulta:')
 labelSelecionarArquivo.grid(row=5, column=0, padx=10, pady=10, sticky='NSEW', columnspan=2)
 
+botaoSelecionarArquivo = tk.Button(text='Selecionar Arquivo', command=SelecionarArquivo)
+botaoSelecionarArquivo.grid(row=5, column=2, padx=10, pady=10, sticky='NSEW')
 
+labelArquivoSelecionado = tk.Label(text='Nenhum arquivo selecionado', anchor='e')
+labelArquivoSelecionado.grid(row=6, column=0, padx=10, pady=10, sticky='NSEW', columnspan=3)
 
+labelDataInicial = tk.Label(text='Data Inicial')
+labelDataInicial.grid(row=7, column=0, padx=10, pady=10, sticky='NSEW')
 
+labelDataFinal = tk.Label(text='Data Final',)
+labelDataFinal.grid(row=8, column=0, padx=10, pady=10, sticky='NSEW')
 
+calendarioDataInicial = DateEntry(year=2022, locale='pt_br')
+calendarioDataInicial.grid(row=7, column=1, padx=10, pady=10, sticky='NSEW')
+
+calendarioDataFinal = DateEntry(year=2022, locale='pt_br')
+calendarioDataFinal.grid(row=8, column=1, padx=10, pady=10, sticky='NSEW')
+
+botaoAtualizarCotacoes = tk.Button(text='Atualizar Cotações', command=AtualizarCotacoes)
+botaoAtualizarCotacoes.grid(row=9, column=0, padx=10, pady=10, sticky='NSEW')
+
+labelCotacaoAtualizada = tk.Label(text='')
+labelCotacaoAtualizada.grid(row=9, column=1, padx=10, pady=10, sticky='NSEW', columnspan=2)
+
+botaoFechar = tk.Button(text='Fechar', command=janela.quit)
+botaoFechar.grid(row=10, column=2, padx=10, pady=10, sticky='NSEW')
 
 janela.mainloop()
